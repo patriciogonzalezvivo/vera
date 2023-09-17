f2236041193986082307005977b60754953ed471
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2023-09-17 13:13:04 -0400

    tuning poisson fill

--- include/vera/shaders/poissonfill.h
+++ include/vera/shaders/poissonfill.h
@@ -51,8 +51,10 @@ varying vec2 v_texcoord;
 
 void main() {
     vec4 color = vec4(0.0);
-    vec2 pixel = u_pixel;
-    vec2 st = v_texcoord;
+    // vec2 pixel = u_pixel;
+    vec2 pixel = 1.0/u_resolution;
+    vec2 st = gl_FragCoord.xy * pixel;
+    // vec2 st = v_texcoord;
 
     if (!u_pyramidUpscaling) {
         // DOWNSCALE
@@ -64,7 +66,7 @@ void main() {
                 vec2 uv = st + vec2(float(dx), float(dy)) * pixel * 0.5;
                 if (uv.x <= 0.0 || uv.x >= 1.0 || uv.y <= 0.0 || uv.y >= 1.0)
                     continue;
-                color += texture2D(u_pyramidTex0, saturate(uv)) * H1(dx) * H1(dy);
+                color += texture2D(u_pyramidTex0, uv) * H1(dx) * H1(dy);
             }
         }
     }
@@ -77,7 +79,9 @@ void main() {
         for (int dy = -1; dy <= 1; dy++) {
             for (int dx = -1; dx <= 1; dx++) {
                 vec2 uv = st + vec2(float(dx), float(dy)) * pixel;
-                color += texture2D(u_pyramidTex0, saturate(uv)) * G(dx) * G(dy);
+                if (uv.x <= 0.0 || uv.x >= 1.0 || uv.y <= 0.0 || uv.y >= 1.0)
+                    continue;
+                color += texture2D(u_pyramidTex0, uv) * G(dx) * G(dy);
             }
         }
 
@@ -86,7 +90,7 @@ void main() {
                 vec2 uv = st + vec2(float(dx), float(dy)) * pixel * 2.0;
                 if (uv.x <= 0.0 || uv.x >= 1.0 || uv.y <= 0.0 || uv.y >= 1.0)
                     continue;
-                color += texture2D(u_pyramidTex1, saturate(uv)) * h2 * H1(dx) * H1(dy);
+                color += texture2D(u_pyramidTex1, uv) * h2 * H1(dx) * H1(dy);
             }
         }
     }
